ee7c1c61bc6a672ddbc04afb8e22e344fd587fc1
slavabusha <32014809+slavabusha@users.noreply.github.com>
2017-09-17 19:48:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]args){
+        int num;
+        int num1;
+        int num2;
+        num1=1;
+        num2=2;
+        num=num1+num2;
+        System.out.println("Сумма двух чисел");
+        System.out.println(num);
+        }
+    }
+
